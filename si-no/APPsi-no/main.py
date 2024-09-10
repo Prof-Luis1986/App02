b9884d4b3424a7167f6cb993be8befdfa1e6a74a
Luis Alfonso Martínez Alfaro <131821493+Prof-Luis1986@users.noreply.github.com>
2024-09-09 18:02:57 -0600

    Se sube la versión final de la app

---
+++ si-no/APPsi-no/main.py
@@ -0,0 +1,38 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "¿Me perdonas?"
+    page.bgcolor="pink"
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    
+    lbl1=ft.Text("¿Me perdonas?",
+                 style=ft.TextStyle(size=40,weight="bold"))
+    img1=ft.Image(src="triste.png",width=200,height=200)
+    
+    btnSi=ft.ElevatedButton("Si",
+                            color="green",
+                            width=100,
+                            height=50
+                            )
+    
+    btnNo=ft.ElevatedButton("No",
+                            color="red",
+                            width=100,
+                            height=50
+                            )
+    page.add(
+        ft.Column(
+            [
+                lbl1,
+                img1,
+                ft.Row(
+                    [btnSi,btnNo]
+                )
+            ]
+        )
+    )
+
+
+ft.app(main)
